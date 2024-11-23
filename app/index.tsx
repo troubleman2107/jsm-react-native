d758bccf4269c91a7261a0bcf9217d75eabdca53
troubleman2107 <jackymad06@gmail.com>
2024-11-23 14:25:29 +0700

    init project

---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
+const App = () => {
+  return (
+    <View className="flex justify-center items-center flex-1">
+      <Text className="text-primary text-3xl font-pblack">Hello Word</Text>
+      <StatusBar style="auto" />
+      <Link href={"/profile"} style={{ color: "blue" }}>
+        Go to profile
+      </Link>
+    </View>
+  );
+};
+
+export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     display: "flex",
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+// });
